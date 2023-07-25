73fd3b5d7eaaaa3381d4cbc8c00bc3e0491d2c69
hlx <lingxu.he@santachi.com.cn>
2023-07-25 11:35:04 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,3 @@
+https://craftinginterpreters.com/
+
+**练习代码，顺带java实现的bytecode vm版本**
---
+++ vm/src/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,13 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * @author hlx
+ * @date 2023-07-25
+ */
+public class Lox {
+
+    public static void main(String[] args)  {
+        System.out.println("vm");
+    }
+
+}
