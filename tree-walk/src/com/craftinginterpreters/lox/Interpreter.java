dac6efc03b0a9ee5a71c80f24cc808c81536f091
hlx <lingxu.he@santachi.com.cn>
2023-08-14 16:31:07 +0800

    control flow 完成

--- tree-walk/src/com/craftinginterpreters/lox/Interpreter.java
+++ tree-walk/src/com/craftinginterpreters/lox/Interpreter.java
@@ -37,7 +37,7 @@ class Interpreter implements Expr.Visitor<Object>,
             for (Stmt statement : statements) {
                 execute(statement);
             }
-        }finally {
+        } finally {
             this.environment = previous;
         }
     }
@@ -51,6 +51,16 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
         return null;
     }
 
@@ -136,6 +146,19 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
@@ -153,6 +176,14 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
         return environment.get(expr.name);
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
