0d9cafd83b4c73bb1e33fe43933fb2a69181b73c
hlx <lingxu.he@santachi.com.cn>
2023-08-14 10:21:30 +0800

    Statements and State 完成

--- tree-walk/src/com/craftinginterpreters/lox/Environment.java
+++ tree-walk/src/com/craftinginterpreters/lox/Environment.java
@@ -8,6 +8,15 @@ import java.util.Map;
  * @date 2023-08-10
  */
 class Environment {
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
+
+    final Environment enclosing;
 
     private final Map<String, Object> values = new HashMap<>();
 
@@ -15,13 +24,29 @@ class Environment {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+
+        if (enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
     void define(String name, Object value) {
         values.put(name, value);
     }
-
 
 }
